070102b516dae5b4e488d40506ca4a65b20ab53b
Deda2206 <deirdrewalsh2206@gmail.com>
2021-02-28 20:39:27 +0000

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./style.css";
+import background from "./images/projectBackground.png";
 
 export default function App() {
   return (
-    <div>
+    <div
+      className="container"
+      styles={{ backgroundImage: `url(${background})` }}
+    >
       <h1>Welcome to Staver</h1>
       <p>Get ready to learn some Music Notation!!</p>
     </div>
